b618ebc5bfa1d550c54b47c27686e074b2342da4
Nyxox-debut <numterminal@gmail.com>
2026-03-18 23:10:46 +0100

    Engine SetUp

--- include/engine/creature/mesh.hpp
+++ include/engine/creature/mesh.hpp
@@ -1,18 +1,22 @@
-// #pragma once
-// #include "../shader/shader.h"
-// #include "vertex.h"
-// #include <glad/glad.h>
-// #include <vector>
-//
-// class Mesh {
-// private:
-//   unsigned int VAO, VBO, EBO;
-//   unsigned int indexCount; // NOTE: Needed for glDrawElements
-//   void setupMesh();
-//
-// public:
-//   std::vector<Vertex> vertices;
-//   std::vector<unsigned int> indices;
-//   Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indicies);
-//   void Draw(Shader &shader) const;
-// };
+#pragma once
+#include "../shader/shader.hpp"
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <vector>
+
+struct Vertex {
+  glm::vec3 Position;
+};
+
+class Mesh {
+private:
+  unsigned int VAO, VBO, EBO;
+  unsigned int indexCount; // NOTE: Needed for glDrawElements
+  void setupMesh();
+
+public:
+  std::vector<Vertex> vertices;
+  std::vector<unsigned int> indices;
+  Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indicies);
+  void Draw(Shader &shader) const;
+};
